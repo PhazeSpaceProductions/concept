08b2373f9632c6481d38989c89fbf0c8625abcec
Andrew McDonnell <bugs@andrewmcdonnell.net>
2016-09-16 14:45:45 +0930

    Update license stuff

---
+++ ScrappySolutions/Exercise 2/index.js
@@ -0,0 +1,104 @@
+/*
+Copyright (c) 2016 Phaze Space Productions Pty. Ltd.
+Copyright (c) 2016 Andrew McDonnell
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/* This construction is an "IIFE" used to prevent accidental duplicate exection of Javascript code. */
+(function() {
+
+// Using three almost identical event handlers is not an efficient way to code.
+// We do it this way here to illustrate the concept
+
+function page1ClickHandler(e) {
+  // This is a debug message, used to help diagnose problems in the code
+  console.log("Inside function page1ClickHandler");
+  // Hide all pages again
+  $('.my-page').hide();
+  // Show the next page
+  $('#page-2').show();
+}
+
+function page2ClickHandler(e) {
+  // This is a debug message, used to help diagnose problems in the code
+  console.log("Inside function page2ClickHandler");
+  // Hide all pages again
+  $('.my-page').hide();
+  // Show the next page
+  $('#page-3').show();
+}
+
+function page3ClickHandler(e) {
+  // This is a debug message, used to help diagnose problems in the code
+  console.log("Inside function page3ClickHandler");
+  // Hide all pages again
+  $('.my-page').hide();
+  // Show the last page
+  $('#page-4').show();
+}
+
+function backButtonHandler(event) {
+  // This is a debug message, used to help diagnose problems in the code
+  console.log("Inside function backButtonHandler");
+
+  // Make sure the page click handler doesnt also get called
+  event.stopPropagation();
+
+  // Here, we "work out" which page we are on and where to go to
+  var pageRef = $(event.target).parent().parent();
+
+  // Strip off the 'page-' - 5 characters, positions 0 through 4 - from the start
+  pageRef = pageRef.prop('id');
+  pageRef = pageRef.substr(5);
+  // Use the MDN to discover other things you can do with strings
+  // e.g. https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+
+  console.log("Inside function backButtonHandler, on page " + pageRef);
+
+  pageRef = pageRef - 1; // go back one page
+
+  // Sanity check nothing has gone wrong
+  if (pageRef >= 1 && pageRef <= 4) {
+    // Hide all pages again
+    $('.my-page').hide();
+    // Show the desired page
+    var id = '#page-' + pageRef;
+    $(id).show();
+  }
+
+}
+
+// This is the first function run after the page HTML file finished loading
+$( document ).ready(function() {
+  console.log("Scrappy Solution #2");
+
+  // Remove the default hidden flag from all things with class 'my-page'
+  $('.my-page').removeAttr('hidden');
+
+  // And hide all using the jquery way of doing it - display:none
+  $('.my-page').hide();
+
+  // Show the first page
+  $('#page-1').show();
+
+  // Set up the click event handlers for the pages
+  $('#page-1').on('click', page1ClickHandler);
+  $('#page-2').on('click', page2ClickHandler);
+  $('#page-3').on('click', page3ClickHandler);
+
+  // Set up the click event handlers for the buttons
+  $('.my-button').on('click', backButtonHandler);
+});
+
+})();
---
+++ ScrappySolutions/README.md
@@ -0,0 +1,19 @@
+The exercise solutions provided here are released under the Apache 2.0 License.
+
+See: http://choosealicense.com/licenses/apache-2.0/
+
+This means if you use these as a starting point, you need to retain the
+copyright attribution to Phaze Space Productions Pty Ltd.
+
+So when you create your own app, you are likely to better off starting with the
+ScrappySkeleton template instead, and building up from scratch, because you will
+likely want to retain IP or require a different license for your own app code.
+
+Essentially, this means your own app should not end up as a 'derivative work'
+of these examples. Pragmatically it means you can copy/paste and then modify bits
+from here into the app you started from the skeleton, and its all good.
+
+It also means, that anyone else wanting to use these examples in a training
+course is required to acknowledge my original copyright.
+
+Also, IANAL so seek your own advice!
